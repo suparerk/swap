54fef3d3842c6df536580e31fcbaa51225f6e482
suparerk <ch.suparerk@gmail.com>
2017-03-03 17:01:58 +0700

    improve code follow eslint errors

--- src/components/AppRoot.jsx
+++ src/components/AppRoot.jsx
@@ -1,75 +1,71 @@
-import React, { Component } from 'react';
-import Card from './card/card.jsx';
+import React, { Component } from 'react'
+import Card from './card/card.jsx'
 
 class AppRoot extends Component {
-  constructor(){
-    super();
+  constructor() {
+    super()
     this.state = {
       input: '',
       shuffled: [],
       pair: [],
       validate: [],
       checked: false,
-      notification: ''
+      notification: '',
     }
-    this.update = this.update.bind(this);
-    this.swap = this.swap.bind(this);
-    this.checkPosition = this.checkPosition.bind(this);
-    this.mark = this.mark.bind(this);
-
+    this.update = this.update.bind(this)
+    this.swap = this.swap.bind(this)
+    this.checkPosition = this.checkPosition.bind(this)
+    this.mark = this.mark.bind(this)
   }
-  update(e){
-    let shuffled_arr = ((input) => {
-      for (let i = input.length; i; i--) {
-        let j = Math.floor(Math.random() * i);
-        [input[i - 1], input[j]] = [input[j], input[i - 1]];
+  update(e) {
+    const shuffledArray = ((input) => {
+      const intputArray = input
+      for (let i = intputArray.length; i; i -= 1) {
+        const j = Math.floor(Math.random() * i);
+        [intputArray[i - 1], intputArray[j]] = [intputArray[j], intputArray[i - 1]]
       }
-      return input;
-    })(Array.from(e.target.value));
+      return intputArray
+    })(Array.from(e.target.value))
 
-    this.setState({input: e.target.value})
-    this.setState({shuffled: shuffled_arr})
-    this.checkPosition(shuffled_arr);
-
+    this.setState({ input: e.target.value })
+    this.setState({ shuffled: shuffledArray })
+    this.checkPosition(shuffledArray)
   }
   swap(c) {
-    let pair = this.state.pair;
-    let shuffled = this.state.shuffled
-    pair.push(c.index);
-    if(pair.length === 2) {
-    ((index_arr) => {
-        let tmp = shuffled[index_arr[0]]
-        shuffled[index_arr[0]] = shuffled[index_arr[1]]
-        shuffled[index_arr[1]] = tmp
-      })(pair);
-      pair = [];
+    let pair = this.state.pair
+    const shuffled = this.state.shuffled
+    pair.push(c.index)
+    if (pair.length === 2) {
+      ((indexArray) => {
+        const tmp = shuffled[indexArray[0]]
+        shuffled[indexArray[0]] = shuffled[indexArray[1]]
+        shuffled[indexArray[1]] = tmp
+      })(pair)
+      pair = []
     }
-    this.setState({pair: pair})
-    this.setState({shuffled: shuffled})
-    this.checkPosition();
+    this.setState({ pair: pair })
+    this.setState({ shuffled: shuffled })
+    this.checkPosition()
   }
-  checkPosition(shuffled){
-    let input = this.state.input
+  checkPosition(shuffled) {
+    const input = this.state.input
     shuffled = shuffled || this.state.shuffled
-    let result = [];
+    const result = []
     shuffled.forEach(((i, index) => {
-        result.push(i === input[index])
-    }));
-    this.setState({validate: result})
-    this.setState({notification: ""})
-
+      result.push(i === input[index])
+    }))
+    this.setState({ validate: result })
+    this.setState({ notification: '' })
   }
-  mark(){
-    if(!this.state.validate.includes(false)){
-      this.setState({notification: "Good job!"})
-    }
-    else
-    {
-      this.setState({notification: "Try again :)"})
+  mark() {
+    if (!this.state.validate.includes(false)) {
+      this.setState({ notification: 'Good job!' })
+    } else {
+      this.setState({ notification: 'Try again :)' })
     }
   }
   render() {
-    let items = this.state.shuffled
+    const items = this.state.shuffled
     return (
       <div className="App w3-content w3-padding-128">
         <div className="w3-row-padding">
@@ -77,17 +73,23 @@ class AppRoot extends Component {
             <h1>Your word here:</h1>
           </div>
           <div className="w3-container w3-twothird">
-            <input type="text"
-              className="w3-input w3-margin-top	"
-              onChange={this.update.bind(this)} />
+            <input
+              type="text"
+              className="w3-input w3-margin-top"
+              onChange={this.update}
+            />
           </div>
         </div>
         <div className="w3-continer w3-padding-128">
           {items.map((item, index) =>
-            <Card key={index} index={index} char={item} swap={this.swap} checked={this.state.validate[index]}/> )}
+            <Card
+              key={index} index={index} char={item}
+              swap={this.swap}
+              checked={this.state.validate[index]}
+            />)}
         </div>
         <div className="w3-continer w3-margin-top w3-row-padding w3-padding-128">
-          <button className="w3-button w3-block w3-teal" onClick={ () => {this.checkPosition(items); this.mark()}}><h2>Mark it</h2></button>
+          <button className="w3-button w3-block w3-teal" onClick={() => { this.checkPosition(items); this.mark() }} ><h2>Mark it</h2></button>
         </div>
         <div className="w3-continer w3-row-padding">
           <div className="w3-panel w3-center w3-red ">
@@ -95,9 +97,9 @@ class AppRoot extends Component {
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
 
 
-export default AppRoot;
+export default AppRoot
--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,34 +1,38 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react'
 
 export default class Card extends Component {
-  
+
   constructor(props) {
-    super();
+    super()
 
     this.state = {
       checkedClass: this.getClassName(props.checked),
     }
   }
 
-  getClassName(checked) {
-    return checked? 'w3-green': 'w3-blue';
-  }
-
   componentWillReceiveProps(newProps) {
-    console.log("newProps", newProps);
-    console.log("checkedClass", this.state.checkedClass);
-    this.setState({checkedClass: this.getClassName(newProps.checked)})
+    this.setState({ checkedClass: this.getClassName(newProps.checked) })
   }
 
+  getClassName(checked) {
+    return checked ? 'w3-green' : 'w3-blue'
+  }
   render() {
     return (
       <div className={`w3-col s2 ${this.state.checkedClass} w3-center w3-card w3-padding-16 w3-margin`}>
         <h1
-        className="w3-padding-32"
-        onClick={() => { console.log('clicked');this.props.swap(this.props) }}>
-        {this.props.char}
+          className="w3-padding-32"
+          onClick={() => { this.props.swap(this.props) }}
+        >
+          {this.props.char}
         </h1>
       </div>
-    );
+    )
   }
+}
+
+Card.propTypes = {
+  checked: React.PropTypes.bool.isRequired,
+  char: React.PropTypes.string.isRequired,
+  swap: React.PropTypes.func.isRequired,
 }
